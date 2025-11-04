36768ab0d2f7b5b7f08c52adc8fc0b1bbd0fbf23
R0binBl00d <collin.sale@icloud.com>
2025-11-04 16:30:59 +0100

    removed commented old class

--- AdventOfCodeNet10/Extensions/LongPoint.cs
+++ AdventOfCodeNet10/Extensions/LongPoint.cs
@@ -45,48 +45,4 @@
     //public override string ToString() => $"x:'{this.x}' y:'{this.y}'";
   }
 
-  //internal class LongPoint
-  //{
-  //  public LongPoint(long X, long Y, int obj = 0)
-  //  {
-  //    this.x = X;
-  //    this.y = Y;
-  //    this.tag = obj;
-  //  }
-
-  //  public long x { get; set; }
-  //  public long y { get; set; }
-  //  public int tag { get; set; }
-
-  //  public void Deconstruct(out long X, out long Y)
-  //  {
-  //    X = this.x;
-  //    Y = this.y;
-  //  }
-
-  //  public override bool Equals(object? obj)
-  //  {
-  //    if (obj is LongPoint other) return other.x == this.x && other.y == this.y;
-  //    return false;
-
-  //    //LongPoint? other = obj as LongPoint;
-  //    //if (other == null) return false;
-  //    //return other.x == this.x && other.y == this.y;
-  //  }
-
-  //  public override int GetHashCode()
-  //  {
-  //    return this.x.GetHashCode() ^ this.y.GetHashCode();
-  //  }
-
-  //  public new Type GetType()
-  //  {
-  //    return typeof(LongPoint);
-  //  }
-
-  //  public override string ToString()
-  //  {
-  //    return $"x:'{x}' y:'{y}'";
-  //  }
-  //}
 }
